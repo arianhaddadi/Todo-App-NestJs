c26afdbaa438388e68f04512c7dcf2d925a21345
Aryan <a94h.73@gmail.com>
2020-12-08 17:09:19 +0330

    fixed api documentation for dtos

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -4,9 +4,9 @@ export default class CreateBookDto {
     @ApiProperty({type: "string" ,description:"Enter book's name.", maxLength: 500}) 
     readonly name: string;
 
-    @ApiPropertyOptional({type: "number" ,description:"Enter the id of the user owning the book."}) 
+    @ApiProperty({type: "number" ,description:"Enter the id of the user owning the book."}) 
     readonly userID: number;
 
-    @ApiPropertyOptional({type: "array", items:{type: "number"} ,description:"Enter ids of the genres associated with the book."}) 
+    @ApiProperty({type: "array", items:{type: "number"} ,description:"Enter ids of the genres associated with the book."}) 
     readonly genreIDs: number[];
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,6 +4,6 @@ export default class CreateUserDto {
     @ApiProperty({type: "string" ,description:"Enter User's Name.", maxLength: 500}) 
     readonly name: string;
 
-    @ApiPropertyOptional({type: "array", items: {type: "number"} ,description:"Enter Book Ids associated with user Name."}) 
+    @ApiProperty({type: "array", items: {type: "number"} ,description:"Enter Book Ids associated with user Name."}) 
     readonly books: number[] ;
 }
