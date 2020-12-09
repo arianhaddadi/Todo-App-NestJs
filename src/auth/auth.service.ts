3b666aa872f39ec4e63031fb3db55566c3de169c
Aryan <a94h.73@gmail.com>
2020-12-10 01:13:07 +0330

    added username and password properties to user entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,4 +4,8 @@ import { UserServices } from 'src/User/user.service';
 @Injectable()
 export class AuthService {
     constructor(private userService: UserServices) {}
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.userService.findOne(username);
+    }
 }
--- src/db/entity/user.entity.ts
+++ src/db/entity/user.entity.ts
@@ -7,6 +7,12 @@ export default class UserEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Column({length: 500})
+    username: string;
+
+    @Column({length: 500})
+    password: string;
+
     @Column({ length: 500 })
     name: string;
 
