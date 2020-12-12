5cb0d218625f237c65e60fe87a9ae1376dfc575e
Aryan <a94h.73@gmail.com>
2020-12-12 19:08:06 +0330

    removed user & genre modules

--- src/genre/dto/create-genre.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'; 
-
-export default class CreateGenreDto {
-    @ApiProperty({type: "string" ,description:"Enter type of the genre."}) 
-    readonly type: string;
-}
