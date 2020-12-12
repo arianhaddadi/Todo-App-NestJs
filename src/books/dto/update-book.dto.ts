c3528ced945047cb993d880982dc4ef0bc5e3f59
Aryan <a94h.73@gmail.com>
2020-12-12 19:08:06 +0330

    removed user & genre modules

--- src/books/dto/update-book.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'; 
-import CreateBookDto from './create-book.dto';
-
-export default class UpdateBookDto extends CreateBookDto {
-    
-    @ApiProperty({type: "number" ,description:"Enter book's id.", maxLength: 500}) 
-    readonly id: number;
-}
