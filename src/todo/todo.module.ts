f529a12650fa0ba1974cf2a7131f29cd5e379016
Aryan <a94h.73@gmail.com>
2020-12-12 20:48:48 +0330

    added jwt authentication requitrement for todo module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [
+    TodoService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ]
 })
 export class TodoModule {}
