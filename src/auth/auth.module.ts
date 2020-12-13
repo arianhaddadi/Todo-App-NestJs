37dfaa8268e49c6787940f5ea6d4d8af0661854f
Aryan <a94h.73@gmail.com>
2020-12-13 14:52:30 +0330

    made jwt token expiration due 60 seconds

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({ 
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1600s' },
+      signOptions: { expiresIn: '60s' },
     })
   ],
   providers: [AuthService, LocalStrategy, UserServices, JwtStrategy],
